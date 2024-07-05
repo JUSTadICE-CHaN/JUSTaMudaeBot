b7f2fe215c04e87f9f1acd89f46d99057cfac9b6
JUSTadICE <88615618+JUSTadICE-CHaN@users.noreply.github.com>
2024-07-05 12:55:49 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,12 +1,12 @@
-Servers = [ 111111111111111, 22222222222,]
+Servers = [ 111111111111111, 22222222222,] #The actual server/guild ids
 Channels = {
-    1: [1.1, 1.2, 1.3], #The first one is the main channel, inside the brackets are the sub channels that have the same timers (roll resets, etc)
+    1: [1.1, 1.2, 1.3], #The first one is the main channel, inside the brackets are the sub channels (channels within the same server) that have the same timers (roll resets, etc), The first channel is also where the daily vote and pokemon rolls will be done in
     2: [2.1, 2.2, 2.3],
 }
 Token = 'YOUR TOKEN HERE'
 Rollcommand = 'ROLL COMMAND HERE (wa/wg/ma/etc)'
 
-AlwaysRoll = True
+AlwaysRoll = True #Whether it continue to roll after a claim or not
 
 Wishlist = ['Zero Two', 
             'Rem', 
