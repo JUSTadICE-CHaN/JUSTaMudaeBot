8e99dcb52f39ba44112fb9c783f42de94a025bac
JUSTadICE <88615618+JUSTadICE-CHaN@users.noreply.github.com>
2024-09-14 23:41:27 +0100

    Update Normal.py added snipekak

--- Normal.py
+++ Normal.py
@@ -215,7 +215,8 @@ class MyClient(discord.Client):
 
         if waifu.is_claimed:
             print(f"❤️ ---- {waifu.kakera} - {waifu} - {waifu.series} - in {channel_name} by {user}")
-            await self.attempt_kakera_snipe(message, waifu)
+            if ((user != self.user.name) and Config.SnipeKak) or user == self.user.name:
+                await self.attempt_claim(waifu, message, main_channel_id)
         else:
             print(f"🤍 ---- {waifu.kakera} - {waifu} - {waifu.series} - in {channel_name} by {user}")
             if ((user != self.user.name) and Config.Snipe) or user == self.user.name:
